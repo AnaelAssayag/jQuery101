f9ea806a20d97438799109a2a314a9b646acf28e
Anael Dahan <anael.assayag@gmail.com>
2017-12-25 13:43:32 +0200

    update

---
+++ main.js
@@ -0,0 +1,25 @@
+//Exercice 1
+
+$('hello')
+
+var element = $('hello');
+console.log(element);
+
+
+// Partner Exercice 1
+$('h1').css('color', 'blue')
+$('.red-div').css('color', 'red')
+
+$('h1').css('color', 'red');
+
+$('li').css('color', 'pink');
+
+$('#brown-div').css('color', 'brown');
+
+// Partner Exercice 2
+
+$('button').click(function () {
+    alert($('#my-input').val());
+  });
+
+// Partner Exercice 3
